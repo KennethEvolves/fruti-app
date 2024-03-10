859af36cf425d26a561fc4a3af880942ad7b16d0
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-03-10 17:03:58 -0600

    Fourth commit: ImageView con imágenes dinámicas

--- app/src/main/java/com/example/frutiapp/MainActivity.java
+++ app/src/main/java/com/example/frutiapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.frutiapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText et_nombre;
+    private ImageView iv_personaje;
+    private TextView tv_bestScore;
+    private MediaPlayer mp;
+
+    int num_aleatorio = (int) (Math.random() * 10);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        et_nombre = (EditText) findViewById(R.id.txt_nombre);
+        iv_personaje = (ImageView) findViewById(R.id.imageView_Personaje);
+        tv_bestScore = (TextView) findViewById(R.id.textView_BestScore);
+
+        int id;
+
+        if(num_aleatorio == 0 || num_aleatorio == 10){
+            id = getResources().getIdentifier("mango", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if (num_aleatorio == 1 || num_aleatorio == 9) {
+            id = getResources().getIdentifier("fresa", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if (num_aleatorio == 2 || num_aleatorio == 8) {
+            id = getResources().getIdentifier("manzana", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if (num_aleatorio == 3 || num_aleatorio == 7) {
+            id = getResources().getIdentifier("sandia", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        } else if (num_aleatorio == 4 || num_aleatorio == 5 || num_aleatorio == 6 ) {
+            id = getResources().getIdentifier("uva", "drawable", getPackageName());
+            iv_personaje.setImageResource(id);
+        }
     }
 }
