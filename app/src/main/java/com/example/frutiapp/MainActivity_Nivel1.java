f1422262f546449ba52775d3911f68caea011eb7
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-03-10 21:42:39 -0600

    Nine commit: Sumas dinámicas del Activity Nivel 1

--- app/src/main/java/com/example/frutiapp/MainActivity_Nivel1.java
+++ app/src/main/java/com/example/frutiapp/MainActivity_Nivel1.java
@@ -1,5 +1,6 @@
 package com.example.frutiapp;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -49,5 +50,46 @@ public class MainActivity_Nivel1 extends AppCompatActivity {
 
         mp_great = MediaPlayer.create(this, R.raw.wonderful);
         mp_bad = MediaPlayer.create(this, R.raw.bad);
+
+        NumAleatorio();
+    }
+
+    public void NumAleatorio(){
+        if (score <= 9) {
+
+            numAleatorio_uno = (int) (Math.random() * 10);
+            numAleatorio_dos = (int) (Math.random() * 10);
+
+            resultado = numAleatorio_uno + numAleatorio_dos;
+
+            if (resultado <= 10) {
+
+                for (int i = 0; i < numero.length; i++){
+                    int id = getResources().getIdentifier(numero[i], "drawable", getPackageName());
+                    if (numAleatorio_uno == i){
+                        iv_Auno.setImageResource(id);
+                    }if(numAleatorio_dos == i) {
+                        iv_Ados.setImageResource(id);
+                    }
+                }
+
+            } else {
+                NumAleatorio();
+            }
+
+        } else {
+            Intent intent = new Intent(this, MainActivity_Nivel2.class);
+
+            string_score = String.valueOf(score);
+            string_vidas = String.valueOf(vidas);
+            intent.putExtra("Jugador", nombre_jugador);
+            intent.putExtra("score", string_score);
+            intent.putExtra("vidas", string_vidas);
+
+            startActivity(intent);
+            finish();
+            mp.stop();
+            mp.release();
+        }
     }
 }
